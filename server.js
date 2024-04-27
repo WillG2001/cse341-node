c71250d609f152d8357250b2dfac283c7ae2e073
“Will <gia19001@byui.edu>
2024-04-26 20:09:37 -0600

    added routes!

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
-const lesson1Controller = require('./controllers/lesson1');
- 
-app.get('/', lesson1Controller.archieRoute);
-app.get('/gwen', lesson1Controller.gwenRoute);
 
 const port = 3000;
+
+app.use('/', require('./routes'));
  
 app.listen(process.env.PORT || port, () => {
   console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
