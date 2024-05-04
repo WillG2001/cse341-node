af96dcbcb8b5032140cee7979ebe9dc4153c3c86
“Will <gia19001@byui.edu>
2024-05-04 14:08:27 -0600

    adding stuff for project 2 (WIP)

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
+require("dotenv").config();
 const express = require('express');
 const app = express();
+const team2controller = require("./controllers/team2.js");
 
-const port = 3000;
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+  next();
+});
 
-app.use('/', require('./routes'));
+//app.use('/', require('./routes','./team2.js'));
+app.get('/professional', team2controller.professionalRoute);
  
-app.listen(process.env.PORT || port, () => {
-  console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+app.listen(process.env.PORT, () => {
+  console.log('Web Server is listening at port ' + (process.env.PORT));
 });
